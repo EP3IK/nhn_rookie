c4c6d09d0a90755fd86b1fb589a89d95d3f27d6d
Jinbae Kim <jbk2012@snu.ac.kr>
2022-01-12 22:01:55 +0900

    GuestbookServiceImpl 클래스의 register 메서드 수정

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,12 +1,23 @@
 package org.zerock.guestbook.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
+@Log4j2
 public class GuestbookServiceImpl implements GuestbookService {
     @Override
     public Long register(GuestbookDTO dto) {
+        log.info("----- DTO 시작 -----");
+        log.info(dto);
+        log.info("----- DTO 끝 -----");
+
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
         // 등록 후에 자동으로 생성되는 guestbook의 gno를 리턴
         return null;
     }
