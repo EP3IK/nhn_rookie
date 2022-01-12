a5362c50794becc70c6fbae278d60d04ac8838ba
Jinbae Kim <jbk2012@snu.ac.kr>
2022-01-12 17:30:48 +0900

    단일 항목 검색 테스트 코드

--- guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -41,10 +48,39 @@ public class GuestbookRepositoryTests {
         if (result.isPresent()) {
             Guestbook guestbook = result.get();
 
-            guestbook.changeContent("executed from updateTest()");
+//            guestbook.changeContent("executed from updateTest()");
 
             guestbookRepository.save(guestbook);
         }
 
     }
+
+    @Test
+    public void testQuery1() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        // Q도메인 클래스를 가져오면 qGuestbook.title처럼 필드를 변수로 사용 가능
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        // SQL WHERE 문에 들어가는 조건을 담는 컨테이너
+        // BooleanBuilder implements com.querydsl.core.types.Predicate
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 필드 값에 존재여부, 대소비교 등의 메서드를 결합해서 조건 생성
+        BooleanExpression expression = qGuestbook.title.contains("0");
+
+        // 만들어진 조건은 and 또는 or로 빌더와 결합
+        builder.and(expression);
+
+        // GuestbookRepository의 부모 인터페이스 QuerydslPredicateExecutor의 findAll 메서드는
+        // Predicate와 Pageable을 인자로 받을 수 있다.
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+
+    }
+
 }
