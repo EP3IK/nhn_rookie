bcb745d8d4ee53c85a859fd1e220bc5750994634
Jinbae Kim <jbk2012@snu.ac.kr>
2022-01-12 22:16:15 +0900

    register 메서드에 DB와 연동되도록 repository 추가

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,6 @@
 package org.zerock.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
@@ -8,7 +9,12 @@ import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+// 의존성 자동 주입: 이거 없으면 GuestbookService 인터페이스에 GuestbookRepository 인스턴스 없다고 에러.
+@RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService {
+    // 반드시 final로 선언: const로 만들어서 바꿔치기를 못하게?
+    private final GuestbookRepository guestbookRepository;
+
     @Override
     public Long register(GuestbookDTO dto) {
         log.info("----- DTO 시작 -----");
@@ -18,7 +24,9 @@ public class GuestbookServiceImpl implements GuestbookService {
         Guestbook entity = dtoToEntity(dto);
         log.info(entity);
 
+        guestbookRepository.save(entity);
+
         // 등록 후에 자동으로 생성되는 guestbook의 gno를 리턴
-        return null;
+        return entity.getGno();
     }
 }
