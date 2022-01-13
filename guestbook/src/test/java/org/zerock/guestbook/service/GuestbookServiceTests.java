c3d57f7b40b767cf4a4de0ad8c27b9aaacc795eb
Jinbae Kim <jbk2012@snu.ac.kr>
2022-01-13 11:22:30 +0900

    서비스의 getList 테스트 코드

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 @SpringBootTest
 public class GuestbookServiceTests {
@@ -19,5 +22,30 @@ public class GuestbookServiceTests {
                 .build();
 
         System.out.println(service.register(dto));
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(31)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> pageResultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("prev: " + pageResultDTO.isPrev());
+        System.out.println("next: " + pageResultDTO.isNext());
+        System.out.println("total page: " + pageResultDTO.getTotalPage());
+
+        System.out.println("----- guestbook DTO 시작 -----");
+//        for (GuestbookDTO guestbookDTO : pageResultDTO.getDtoList()) {
+//            System.out.println(guestbookDTO);
+//        }
+        pageResultDTO.getDtoList().forEach(System.out::println);
+        System.out.println("----- guestbook DTO 끝 -----");
+
+        System.out.println("----- 페이지 목록 시작 -----");
+        pageResultDTO.getPageList().forEach(System.out::println);
+        System.out.println("----- 페이지 목록 끝 -----");
     }
 }
